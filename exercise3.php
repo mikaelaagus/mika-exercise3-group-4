cff53158cf4fc32a4cdb0a6bdaf7169740e68d16
mikaelaagus <mikaelamedinaagus@gmail.com>
2024-09-11 22:05:28 +0800

    Add files via upload

---
+++ exercise3.php
@@ -0,0 +1,20 @@
+<DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width.initail-scale=1.0">
+    <title>Exercise3</title>
+</head>
+<body>
+<?php
+       file_get_contents('Text.txt');
+       "<brb>";
+       file_put_contents(Newfile.txt',hi, we are a group 4 from BSIT3F');
+       '<brb>';
+       file_exists('Text.txt');
+       '<brb>';
+       print_r(file('Text.txt',));
+?>
+
+</body>
+</html>
